5b918a252f07d41bbbea04184fdc6913eb112d8e
Joss2416 <36967170+Joss2416@users.noreply.github.com>
2018-10-24 03:03:42 +0700

    Update safelink_utama.js

--- safelink_utama.js
+++ safelink_utama.js
@@ -21,10 +21,10 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="dos-and-donts-when-completing.html"}
-if(x=="2"){xxx="optimal-circulatory-health-with-natural.html"}
-if(x=="3"){xxx="medical-home-or-health-hub-new-health.html"}
-if(x=="4"){xxx="affordable-health-insurance-makes.html"}a_to_ve[i].href="https://safe.mbfileshare.com/2018/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="affordable-health-insurance-makes.html"}
+if(x=="2"){xxx="medical-home-or-health-hub-new-health.html"}
+if(x=="3"){xxx="optimal-circulatory-health-with-natural.html"}
+if(x=="4"){xxx="dos-and-donts-when-completing.html"}a_to_ve[i].href="https://safe.mbfileshare.com/2018/10/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
